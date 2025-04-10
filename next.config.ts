f2892e237e4f4eca66ab4d3d1ed3008d183fa5e0
Shubham kumar kushwaha <shubhamkush012@gmai.com>
2025-04-10 23:24:52 +0530

    some ereor fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    allowedDevOrigins: [
+      "https://8e81-3-141-7-76.ngrok-free.app",
+    ],
+  },
 };
 
 export default nextConfig;
